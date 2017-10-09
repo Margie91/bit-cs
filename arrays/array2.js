60bfc8fa444cbc5aac0f0be8495dc1464d485c5f
BIT <classroom@bgit.rs>
2017-10-09 18:03:47 +0200

    loop exercises

--- arrays/array2.js
+++ arrays/array2.js
@@ -3,3 +3,9 @@ var c = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26
 console.log(c[24]);
 console.log(c[3]);
 console.log(c[2][1]);
+
+delete c[2][1];
+console.log(c);
+
+c[2][4] = 'string';
+console.log(c);
