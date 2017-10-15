f0bef3ee35690eca14ace8f49408e6432a9b6236
Marija Nikolic <m.nikolic1144@gmail.com>
2017-10-15 15:49:21 +0200

    functions homework

---
+++ functions_homework/functions1.js
@@ -0,0 +1,18 @@
+function insertString(string1, string2, position) {
+    if (position == 0 || position !== 'number') {
+        position = 1;
+    }
+    var result = '';
+    for (var i = 0; i < string1.length; i++) {
+        if (i == position - 1) {
+            for (var j = 0; j < string2.length; j++) {
+                result += string2[j];
+            }
+        }
+        result += string1[i];
+
+    }
+    return result;
+}
+
+console.log(insertString('Mystring', 'JS'));
