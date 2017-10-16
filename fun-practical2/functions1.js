06c748aaaf9cd01d652238f1d9b59de737953386
BIT Student <classroom@bgit.rs>
2017-10-16 16:57:40 +0200

    NOVI ZADACI!!!

---
+++ fun-practical2/functions1.js
@@ -0,0 +1,16 @@
+function countVowels(str) {
+    var string = str.toLowerCase();
+    var vowels = 0;
+
+    for (var i = 0; i < string.length; i++) {
+        if (string[i] == 'a' || string[i] == 'e' || string[i] === 'i' || string[i] == 'o' || string[i] == 'u') {
+            vowels++;
+        }
+        
+    }
+
+    return vowels;
+
+}
+
+console.log(countVowels('Marija'));
